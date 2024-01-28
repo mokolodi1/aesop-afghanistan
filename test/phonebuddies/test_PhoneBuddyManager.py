6e1148ea236e9aa6eb393e4293323d9547e22c32
Ian Norris <iannorris@Ians-iMac.local>
2024-01-28 10:39:32 -0600

    Add rest of tests in #30

--- test/phonebuddies/test_PhoneBuddyManager.py
+++ test/phonebuddies/test_PhoneBuddyManager.py
@@ -25,6 +25,9 @@ class TestPhoneBuddyManager(unittest.TestCase):
 
         self.mock_email_info = EmailInfo("Intro", "Topic")
 
+        self.mock_email = "email1@gmail.com"
+        self.mock_message = "admin message"
+
         self.today_process_date = datetime.now().strftime("%A, %B %d, %Y")
 
 
@@ -87,7 +90,44 @@ class TestPhoneBuddyManager(unittest.TestCase):
         process_row_data = [self.today_process_date] + ['yes','yes','yes','yes','Yes','Yes']
         db_instance = mock_db_connector.get_instance.return_value
         db_instance.get_this_weeks_process.return_value = WeeklyProcess.parse_from_google_sheet_row(process_row_data, 0)
+        email_sender_instance = mock_email_sender.get_instance.return_value
 
+        # Set up the object and perform the test
+        manager = PhoneBuddyManager(really_send_emails=False, is_robot=True)
+        manager.manage_phone_buddy_list()
+
+        # Verify that no emails were sent to the two admins and that the result is correct
+        self.assertEqual(email_sender_instance.send_email.call_count, 0)
+        self.assertEqual(ResultTracker.get_result(), "Phone buddy list already sent this week.")
+
+
+#second new test
+
+@patch('phonebuddies.PhoneBuddyManager.EmailSender')
+@patch('phonebuddies.PhoneBuddyManager.DatabaseConnector')
+def test_manage_phone_buddy_list_send_admin_trial_emails(self, mock_db_connector, mock_email_sender):
+        process_row_data = [self.today_process_date] + ['yes','yes','yes','yes','Yes','Yes']
+        db_instance = mock_db_connector.get_instance.return_value
+        db_instance.get_this_weeks_process.return_value = WeeklyProcess.parse_from_google_sheet_row(process_row_data, 0)
+        email_sender_instance = mock_email_sender.get_instance.return_value
+
+        # Set up the object and perform the test
+        manager = PhoneBuddyManager(really_send_emails=False, is_robot=True)
+        manager.manage_phone_buddy_list()
+
+        # Verify that emails were sent to the two admins and that the draft buddy email is correct
+        self.assertEqual(email_sender_instance.send_email.call_count, 2)
+        self.assertEqual(EmailDraft(email, "ACTION NEEDED: AESOP Phone Buddy Reminder", message), "email1@gmail.com", ACTION NEEDED: AESOP Phone Buddy Reminder","admin message")
+
+#third new test
+
+"""
+@patch('phonebuddies.PhoneBuddyManager.EmailSender')
+@patch('phonebuddies.PhoneBuddyManager.DatabaseConnector')
+def test_manage_phone_buddy_list_send_buddy_emails(self, mock_db_connector, mock_email_sender):
+        process_row_data = [self.today_process_date] + ['yes','yes','yes','yes','Yes','Yes']
+        db_instance = mock_db_connector.get_instance.return_value
+        db_instance.get_this_weeks_process.return_value = WeeklyProcess.parse_from_google_sheet_row(process_row_data, 0)
         email_sender_instance = mock_email_sender.get_instance.return_value
 
         # Set up the object and perform the test
@@ -97,6 +137,31 @@ class TestPhoneBuddyManager(unittest.TestCase):
         # Verify that emails were sent to the two admins and that the result is correct
         self.assertEqual(email_sender_instance.send_email.call_count, 0)
         self.assertEqual(ResultTracker.get_result(), "Phone buddy list already sent this week.")
+"""
+#fourth new test
+
+"""
+@patch('phonebuddies.PhoneBuddyManager.EmailSender')
+@patch('phonebuddies.PhoneBuddyManager.DatabaseConnector')
+def test_manage_phone_buddy_list_error(self, mock_db_connector, mock_email_sender):
+        process_row_data = [self.today_process_date] + ['yes','yes','yes','yes','Yes','Yes']
+        db_instance = mock_db_connector.get_instance.return_value
+        db_instance.get_all_buddies_map.return_value = self.mock_buddy_map
+        
+        #Only use the first item from self.mock_buddy_pairs so get_buddy_email_pairs returns an error
+        db_instance.get_buddy_email_pairs.return_value = self.mock_buddy_pairs[0]
+        db_instance.get_this_weeks_process.return_value = WeeklyProcess.parse_from_google_sheet_row(process_row_data, 0)
+
+        email_sender_instance = mock_email_sender.get_instance.return_value
+
+        # Set up the object and perform the test
+        manager = PhoneBuddyManager(really_send_emails=False, is_robot=True)
+        manager.manage_phone_buddy_list()
+
+        # Verify that emails were sent to the two admins and that the result is correct
+        self.assertEqual(email_sender_instance.send_email.call_count, 1)
+        self.assertEqual(ResultTracker.get_result(), "No data in database for email in pair "email1@gmail.com" : "email3@gmail.com%")
 
 if __name__ == '__main__':
     unittest.main()
+"""
